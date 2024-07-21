3ec632876653f7ac09fff1e5ef430acbae00ee70
akshayaca <akshayaca@outlook.com>
2024-07-21 15:07:17 -0400

    Added CORS code

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Use cors middleware
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
